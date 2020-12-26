b19babd19c496ad765f18d8698b84a4dd3d0b763
unknown <yossi.povalaev@gmail.com>
2020-12-26 21:17:54 +0200

    make it better

---
+++ frontend/src/actions/scriptt.js
@@ -0,0 +1,23 @@
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function (todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+        }
+    });
+}
